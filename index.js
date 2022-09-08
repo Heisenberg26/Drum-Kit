56e37db9fc8c825e4641af843df4477aa8f1dcba
Ashutosh Devu <devuashutosh26@gmail.com>
2022-09-08 20:28:24 +0530

    Adding Loop inorder to incorporate event listener to every button

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
-document.querySelector("button").addEventListener("click",myFunction);
+var arr = document.querySelectorAll("button").length;
 
+for (let index = 0; index < arr; index++) {
+    
+document.querySelectorAll("button")[index].addEventListener("click",myFunction);
+}
 function myFunction()
 {
     alert("You have clicked the button");
